56221d9a1e1671a88078bac1d1c8e756c8ed3990
fyeh0 <fangiyeh@gmail.com>
2019-04-21 15:00:00 -0400

    number generator

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,43 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
+    // variables
+    var wins = 0;
+    var losses = 0;
+    var randomNumber = Math.floor(Math.random() * 120);
+
+    /* 
     $("#number-generator").text("click", function() {
-        var randomNumber = Math.floor(Math.random() * 120) - 19;
+        var randomNumber = Math.floor(Math.random() * 120);
         console.log(randomNumber);
-    });
+    });*/
+
+    // random number generator
+    console.log("Random number is " + randomNumber);
+
+    var userGuess = 0
+
+    $("#number-generator").text(randomNumber);
+    
+    $("#amethyst").on("click", function () {
+        userGuess = $(Math.floor(Math.random() * 12));
+        console.log(userGuess);
+    })
+
+    $("#aquamarine").click(function () {
+        console.log($(this).val());
+    })
+
+    $("#peridot").click(function () {
+        console.log($(this).val());
+    })
+
+    $("#ruby").click(function () {
+        console.log($(this).val());
+    })
+
+    // if user guesses correct (user guess === randomnumber), win++ and game restarts
+    // if user guesses wrong (user guess != randomenumber), losses++ and game restarts
+
+    // game start, random number generated
+    // user crystal clicks generate random value
 })
